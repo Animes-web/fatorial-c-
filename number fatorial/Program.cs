c8149578bc908ed71d66b630bea0a5ed5f2e9c36
ウェンベル <wenvelk@gmail.com>
2025-03-13 08:36:05 -0300

    Add files via upload

---
+++ number fatorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número para calcular o fatorial dele ");
+        int numero = int.Parse(Console.ReadLine());
+
+        BigInteger fatorial = 1;
+        for (int i = numero; i > 1; i--)
+        {
+            fatorial *= i;
+        }
+
+        Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+    }
+}
